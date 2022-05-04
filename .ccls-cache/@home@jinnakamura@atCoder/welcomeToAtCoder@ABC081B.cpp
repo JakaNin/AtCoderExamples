df16d835304d3ecb14cc42a4f5e1068f224e3793
JinNakamura <jin.nakamura@vitalize.co.jp>
2022-05-05 02:57:42 +0900

    tried practice_1

---
+++ .ccls-cache/@home@jinnakamura@atCoder/welcomeToAtCoder@ABC081B.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+  int N;
+  int a[200];
+
+  int count = 0;
+  cin >> N;
+  for (int i = 0; i < N; i++) cin >> a[i];
+  while (true){
+    bool odd = false;
+    for (int i = 0; i < N; i++){
+      if (a[i] % 2 != 0){
+        odd = true;
+      }
+      if (odd){
+        break;
+      }
+    }
+    for (int i=0; i < N; i++){
+      a[i] /= 2;
+    }
+    count++;
+  }
+  cout << count << endl;
+}
